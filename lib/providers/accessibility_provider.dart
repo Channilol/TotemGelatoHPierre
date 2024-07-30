b0bf34923e44a6c18fe527d607991527a4af4924
Your Name <you@example.com>
2024-07-30 12:16:18 +0200

    aa

--- lib/providers/accessibility_provider.dart
+++ lib/providers/accessibility_provider.dart
@@ -6,7 +6,7 @@ class AccessibilityProvider extends StateNotifier<bool> {
   void changeCategory(bool newParam) => state = newParam;
 }
 
-final categoryProvider =
+final accessibilityProvider =
     StateNotifierProvider<AccessibilityProvider, bool>((ref) {
   return AccessibilityProvider();
 });
